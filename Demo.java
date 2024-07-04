0db937b1ccfd3d59024038209810a1433dbb8c73
masanam444 <masanamm677@gmail.com>
2024-07-04 12:32:01 +0530

    changes will be appiled for Demo.java

--- Demo.java
+++ Demo.java
@@ -1,7 +1,13 @@
+import java.io.DataInputStream;
 class Demo
 {
 public static void main(String[] args)
 {
 	System.out.println("WELCOME");
+	DataInputStream inp=new DataInputStream(System.in);
+	int a=Intger.parseInt(inp.readLine());
+	System.out.println("Given number is :"+a);
+	
+	
 }
-}
+
